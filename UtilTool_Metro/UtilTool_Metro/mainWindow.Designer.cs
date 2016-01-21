0fa44861642f609620c19d7b4ac2201e7a562147
Callum Donovan <vcallumxd@hotmail.co.uk>
2016-01-21 00:50:47 +0000

    Main form changed to attribute Metro Style.

--- UtilTool_Metro/UtilTool_Metro/mainWindow.Designer.cs
+++ UtilTool_Metro/UtilTool_Metro/mainWindow.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace UtilTool_Metro
 {
-    partial class Form1
+    partial class mainWindow
     {
         /// <summary>
         /// Required designer variable.
@@ -28,12 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // mainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(519, 441);
+            this.Name = "mainWindow";
+            this.Text = "mainWindow";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
     }
 }
-
--- UtilTool_Metro/UtilTool_Metro/mainWindow.cs
+++ UtilTool_Metro/UtilTool_Metro/mainWindow.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace UtilTool_Metro
 {
-    public partial class Form1 : Form
+    public partial class mainWindow : MetroFramework.Forms.MetroForm
     {
-        public Form1()
+        public mainWindow()
         {
             InitializeComponent();
         }
